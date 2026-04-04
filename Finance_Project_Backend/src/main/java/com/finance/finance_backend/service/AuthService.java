3cb9b216c4be5ca8996e735addfd6ef6617262d6
moksha <gompasaikumar7@gmail.com>
2026-04-04 20:46:54 +0530

    feat(phase-3): Implement secure JWT authentication and login APIs

---
+++ Finance_Project_Backend/src/main/java/com/finance/finance_backend/service/AuthService.java
@@ -0,0 +1,62 @@
+package com.finance.finance_backend.service;
+
+import com.finance.finance_backend.dto.AuthRequest;
+import com.finance.finance_backend.dto.AuthResponse;
+import com.finance.finance_backend.dto.RegisterRequest;
+import com.finance.finance_backend.entity.Role;
+import com.finance.finance_backend.entity.User;
+import com.finance.finance_backend.repository.UserRepository;
+import com.finance.finance_backend.security.CustomUserDetails;
+import com.finance.finance_backend.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.VIEWER) 
+                .active(true)
+                .build();
+                
+        userRepository.save(user);
+        var customUserDetails = new CustomUserDetails(user);
+        var jwtToken = jwtService.generateToken(customUserDetails);
+        return AuthResponse.builder()
+                .token(jwtToken)
+                .email(user.getEmail())
+                .role(user.getRole().name())
+                .build();
+    }
+
+    public AuthResponse authenticate(AuthRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var customUserDetails = new CustomUserDetails(user);
+        var jwtToken = jwtService.generateToken(customUserDetails);
+        return AuthResponse.builder()
+                .token(jwtToken)
+                .email(user.getEmail())
+                .role(user.getRole().name())
+                .build();
+    }
+}
